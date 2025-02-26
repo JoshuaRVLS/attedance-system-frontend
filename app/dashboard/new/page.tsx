ba79fc7efc779ccfb44010870c20d72f4a2b92c2
Joshua  Ravael <jravaellnew@gmail.com>
2025-02-26 19:03:33 +0700

    Fix Lag when scrolling

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -1,48 +1,54 @@
-'use client';
+"use client";
 
-import axios from "axios"
+import axios from "axios";
 import { FormEvent, useState, useEffect } from "react";
 import { Class } from "@/app/types/class";
 
 export default function NewStudent() {
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
   const [studentClasses, setStudentClasses] = useState<Class[]>([]);
-  const [selectedClass, setSelectedClass] = useState<string>('');
+  const [selectedClass, setSelectedClass] = useState<string>("");
   const [file, setFile] = useState<Blob>();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData();
-    
-    formData.append('firstName', firstName);
-    formData.append('lastName', lastName);
-    formData.append('studentClassId', selectedClass);
-    formData.append('file', file as Blob);
-    
-    const response = await axios.post(`http://192.168.100.7:8000/api/v1/students`, formData, {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'multipart/form-data'
+
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+    formData.append("studentClassId", selectedClass);
+    formData.append("file", file as Blob);
+
+    const response = await axios.post(
+      `http://192.168.100.7:8000/api/v1/students`,
+      formData,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       }
-    });
+    );
 
     if (response.status === 201) {
       console.log(response.data);
-      alert('Success');
+      alert("Success");
     }
-  }
+  };
 
   const getClasses = async () => {
     try {
-      const response = await axios.get('http://192.168.100.7:8000/api/v1/classes');
+      const response = await axios.get(
+        "http://192.168.100.7:8000/api/v1/classes"
+      );
       if (response.status === 200) {
         setStudentClasses(response.data);
       }
-    } catch (error) { 
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getClasses();
@@ -51,17 +57,44 @@ export default function NewStudent() {
   return (
     <div>
       <form onSubmit={handleSubmit} className="flex flex-col gap-2 text-black">
-        <input required placeholder="Nama Depan" type="text" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        <input required placeholder="Nama Belakang" type="text" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        <select value={selectedClass} onChange={e => setSelectedClass(e.target.value)}>
+        <input
+          required
+          placeholder="Nama Depan"
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          required
+          placeholder="Nama Belakang"
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <select
+          value={selectedClass}
+          onChange={(e) => setSelectedClass(e.target.value)}
+        >
           <option value="">Pilih kelas</option>
-          {studentClasses && studentClasses.map(({ id, value }, idx) => (
-            <option key={idx} value={id}>{value}</option>
-          ))}
+          {studentClasses &&
+            studentClasses.map(({ id, value }, idx) => (
+              <option key={idx} value={id}>
+                {value}
+              </option>
+            ))}
         </select>
-        <input className="text-white" type="file" name="file" onChange={(e) => setFile(e.target.files?.[0])} />
-        <button className="text-white" type="submit">Submit</button>
+        <input
+          className="text-white"
+          type="file"
+          name="file"
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+        <button className="text-white" type="submit">
+          Submit
+        </button>
       </form>
     </div>
-  )
+  );
 }
--- app/providers/SocketIOProvider.tsx
+++ app/providers/SocketIOProvider.tsx
@@ -1,43 +1,51 @@
-'use client';
+"use client";
 
 import React, { useState, useEffect } from "react";
 import { SocketIOContext } from "../contexts/SocketIOContext";
 import io, { Socket } from "socket.io-client";
 import { Student } from "../types/student";
 
-const SocketIOProvider = ({children}: {children: React.ReactNode}) => {
-    const [lastMessage, setLastMessage] = useState<string | null>(null);
-    const [lastStudent, setLastStudent] = useState<Student | null>(null);
-    const [socket, setSocket] = useState<typeof Socket|null>(null);
-      
+const SocketIOProvider = ({ children }: { children: React.ReactNode }) => {
+  const [lastMessage, setLastMessage] = useState<string | null>(null);
+  const [lastStudent, setLastStudent] = useState<Student | null>(null);
+  const [socket, setSocket] = useState<typeof Socket | null>(null);
 
-    useEffect(() => {
-        const newSocket = io('http://192.168.100.7:8000');
-        newSocket.on('connect', () => {
-            console.log('connected');
-            setSocket(newSocket);
-        });
+  useEffect(() => {
+    const newSocket = io("http://192.168.100.7:8000");
+    newSocket.on("connect", () => {
+      console.log("connected");
+      setSocket(newSocket);
+    });
 
-        newSocket.on('newPresentStudent', (student: Student) => {
-            setLastMessage('newPresentStudent');
-            console.log(student);
-            setLastStudent(student);
-        })
+    newSocket.on("newPresentStudent", (student: Student) => {
+      setLastMessage("newPresentStudent");
+      console.log(student);
+      setLastStudent(student);
+    });
 
-        newSocket.on('newStudent', () => {
-            setLastMessage('newStudent');
-        })
-    }, []);
+    newSocket.on("newStudent", () => {
+      setLastMessage("newStudent");
+    });
+  }, []);
 
-    const sendMessage = (message: string) => {
-        socket?.emit('message', message);
-    }
+  const sendMessage = (message: string) => {
+    socket?.emit("message", message);
+  };
 
-    return (
-        <SocketIOContext.Provider value={{socket, lastMessage, sendMessage, setLastMessage, lastStudent, setLastStudent}}>
-            {children}
-        </SocketIOContext.Provider>
-    )
-}
+  return (
+    <SocketIOContext.Provider
+      value={{
+        socket,
+        lastMessage,
+        sendMessage,
+        setLastMessage,
+        lastStudent,
+        setLastStudent,
+      }}
+    >
+      {children}
+    </SocketIOContext.Provider>
+  );
+};
 
 export default SocketIOProvider;
