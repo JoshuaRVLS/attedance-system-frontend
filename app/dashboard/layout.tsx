80881ad500d03b468e318a0509f65575ceddd609
Joshua  Ravael <jravaellnew@gmail.com>
2025-02-28 13:51:23 +0700

    Fix width and height not 100%

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,24 +1,26 @@
 import { Metadata } from "next";
 import Navbar from "../components/Navbar";
-import '@/app/globals.css';
+import "@/app/globals.css";
 import Footer from "../components/Footer";
 import SocketIOProvider from "../providers/SocketIOProvider";
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
-    description: 'View and manage your students.'
-}
+  title: "Dashboard",
+  description: "View and manage your students.",
+};
 
 export default function StudentsLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return <div>
-        <SocketIOProvider>
-            <Navbar />
-            {children}
-            <Footer />
-        </SocketIOProvider>
-    </div>;
+  return (
+    <div className="w-full h-full">
+      <SocketIOProvider>
+        <Navbar />
+        {children}
+        <Footer />
+      </SocketIOProvider>
+    </div>
+  );
 }
