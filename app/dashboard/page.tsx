fd41ce6c5ef96e59156a60dac226868faedf83d5
Joshua  Ravael <jravaellnew@gmail.com>
2025-02-28 13:51:39 +0700

    Use motion for in-out transition

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,19 +3,22 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import StudentCard from "../components/StudentCard";
-import { Student } from "../types/student";
 import useSocketIO from "../contexts/SocketIOContext";
 import LatestScannedStudent from "../components/LatestScannedStudent";
 import Menu from "../components/Menu";
+import { motion, AnimatePresence } from "motion/react";
+import Loading from "../components/Loading";
+import { useStudentStore } from "../stores/studentStore";
+import { useSelectedClassStore } from "../stores/selectedClassStore";
 
 export default function Dashboard() {
-  const [students, setStudents] = useState<Student[] | null>(null);
+  const { students, setStudents } = useStudentStore();
   const { lastMessage, setLastMessage } = useSocketIO()!;
-  const [selectedClass, setSelectedClass] = useState<string>("");
+  const { selectedClass } = useSelectedClassStore();
+  const [showQR, setShowQR] = useState<boolean>(false);
 
   const getUsers = async () => {
     try {
-      console.log(selectedClass);
       const response = await axios.get(
         selectedClass
           ? `http://192.168.100.7:8000/api/v1/classes/${selectedClass}`
@@ -30,26 +33,32 @@ export default function Dashboard() {
   };
 
   useEffect(() => {
+    setStudents([]);
     getUsers();
     setLastMessage("");
   }, [lastMessage, setLastMessage, selectedClass]);
 
   return (
-    <div className="flex mt-20 items-center gap-12 justify-center">
-      <Menu
-        selectedClass={selectedClass}
-        setSelectedClass={setSelectedClass}
-        students={students}
-        setStudents={setStudents}
-      />
-      <div className="flex p-10 flex-col rounded-lg gap-3 max-h-[80dvh] min-w-[40rem] max-w-[40rem] min-h-[80dvh] overflow-scroll scrollbar-hide shadow-xl border border-black">
-        {students && students.length === 0 && "No Students"}
-        {!students
-          ? "Loading User"
-          : students.map((student) => (
-              <StudentCard key={student.id} student={student} />
-            ))}
-      </div>
+    <div className="flex h-full items-center gap-12 justify-center ">
+      <Menu showQR={showQR} setShowQR={setShowQR} />
+      <motion.div
+        animate={{
+          transition: {
+            staggerChildren: 0.3,
+          },
+        }}
+        className="relative flex p-10 flex-col rounded-lg gap-3 max-h-[80dvh] min-w-[40rem] max-w-[40rem] min-h-[80dvh] overflow-scroll scrollbar-hide shadow-xl border border-black"
+      >
+        <AnimatePresence>
+          {!students?.length ? (
+            <Loading duration={0.2} text="Loading Students" />
+          ) : (
+            students.map((student) => (
+              <StudentCard showQR={showQR} key={student.id} student={student} />
+            ))
+          )}
+        </AnimatePresence>
+      </motion.div>
       <LatestScannedStudent />
     </div>
   );
