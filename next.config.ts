0c47998b1052e570ea10b14ebf92b8e4f041ba81
Joshua  Ravael <jravaellnew@gmail.com>
2025-04-02 22:58:49 +0700

    Idk

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig: NextConfig = {
   publicRuntimeConfig: {
     backendUrl: "${process.env.NEXT_PUBLIC_API_URL}:8000",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
